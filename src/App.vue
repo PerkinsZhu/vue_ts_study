06cdc688f8d63b34a64726be4f4361d14aa324c3
Perkins <perkinszhu@sina.cn>
2023-06-17 20:39:54 +0800

    结构封装

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,11 @@
 <template>
-  <div class="box">
-    I AM APP ROOT
-    <el-row class="mb-4">
-      <el-button>Default</el-button>
-      <el-button type="primary">Primary</el-button>
-      <el-button type="success">Success</el-button>
-      <el-button type="info">Info</el-button>
-      <el-button type="warning">Warning</el-button>
-      <el-button type="danger">Danger</el-button>
-      <Test></Test>
-
-      <hr />
-      <svg>
-        <use xlink:href="#icon-phone" fill="yellow"></use>
-      </svg>
-
-      <svg-icon name="phone" color="red"></svg-icon>
-    </el-row>
-  </div>
+  <SvgStudy></SvgStudy>
 </template>
 
 <script setup lang="ts">
-import Test from '@/components/Test.vue'
-import SvgIcon from '@/components/SvgIcon/index.vue'
+/**学习 SVG配置及element-plus使用 */
+import SvgStudy from "@/pages/svgStudy/index.vue"
 
-console.log(import.meta.env)
 </script>
 
-<style scoped lang="scss">
-.box {
-  font-weight: 100px;
-}
-.svg {
-  width: 100px;
-  height: 50px;
-}
-</style>
+<style scoped lang="scss"></style>
---
+++ src/pages/svgStudy/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="box">
+        I AM APP ROOT
+        <el-row class="mb-4">
+            <el-button>Default</el-button>
+            <el-button type="primary">Primary</el-button>
+            <el-button type="success">Success</el-button>
+            <el-button type="info">Info</el-button>
+            <el-button type="warning">Warning</el-button>
+            <el-button type="danger">Danger</el-button>
+            <Test></Test>
+
+            <hr />
+            <svg>
+                <use xlink:href="#icon-phone" fill="yellow"></use>
+            </svg>
+
+            <svg-icon name="phone" color="red"></svg-icon>
+        </el-row>
+    </div>
+</template>
+
+<script setup lang='ts'>
+import Test from '@/components/Test.vue'
+import SvgIcon from '@/components/SvgIcon/index.vue'
+
+
+console.log(import.meta.env)
+</script> 
+
+<style scoped>
+.box {
+    font-weight: 100px;
+}
+
+.svg {
+    width: 100px;
+    height: 50px;
+}
+</style>`
