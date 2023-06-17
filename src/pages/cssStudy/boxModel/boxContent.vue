4d9897af42ecea0a79315b6010638f9af9ed781c
Perkins <perkinszhu@sina.cn>
2023-06-17 21:38:22 +0800

    min&max

---
+++ src/pages/cssStudy/boxModel/boxContent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="base">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, numquam. Aliquid, quasi voluptatibus aliquam
+        repudiandae tempora suscipit tenetur fuga modi dolor, tempore rem sequi, consectetur sapiente. Qui voluptatem
+        nostrum eaque.</div>
+</template>
+
+<script setup lang='ts'>
+
+</script> 
+
+<style scoped lang='scss'>
+#base {
+    background-color: slateblue;
+
+
+
+    // 通过调整视口，div的宽和高可以自动适配。但是由于浏览器的限制，宽度不能一直小到零
+    min-width: 500px;
+    max-width: 800px;
+
+    min-height: 200px;
+    max-height: 600px;
+
+
+    
+}
+</style>
--- src/pages/cssStudy/boxModel/index.vue
+++ src/pages/cssStudy/boxModel/index.vue
@@ -1,14 +1,19 @@
 <template>
-  <div>盒子模型分析</div>
+  <div class="base">盒子模型分析</div>
 
   <br />
   <img src="./盒子布局.png" alt="#" />
+
+  <BoxContent></BoxContent>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import BoxContent from './boxContent.vue';
+
+</script>
 
 <style scoped lang="scss">
-div {
+.base {
   background-color: skyblue;
 
   // width 和 height 设置的是内容的宽高
@@ -22,6 +27,9 @@ div {
 
   //边框是压在内边距上面的 虚线可看出来 未遮挡部分的背景颜色依旧是 skyblue  ||| 设置的背景颜色会填充到边框区域
   border: 10px dashed black;
+
+  // 盒子的宽高 = 内容+padding+border
+  //而外边距，不会占用盒子的宽度，属于外部距离
 
   //  外边距 ，只能影响位置。不能影响大小
   margin: 50px;
