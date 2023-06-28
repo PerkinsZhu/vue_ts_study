42068e9d080d5ff39fefe551c86f5e0a93ee044d
PerkinsZhu <perkinszhu@sina.cn>
2023-06-28 18:27:47 +0800

    集成mock

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // 引入svg
 import {createSvgIconsPlugin} from 'vite-plugin-svg-icons'
+// 引入mock
+import {viteMockServe} from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         vue(),
+        // 配置svg插件
         createSvgIconsPlugin({
             // 配置svg存放的文件夹
             iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
             symbolId: 'icon-[dir]-[name]',
+        }),
+        viteMockServe({
+            mockPath: 'mock',
+            localEnabled: true,
+            supportTs: false,
         }),
     ],
     resolve: {
