f263592be9cf9a0bea9eb448e5529c2fd3c91b7e
PerkinsZhu <perkinszhu@sina.cn>
2023-07-01 10:00:22 +0800

    add layout

---
+++ src/layout/index.vue
@@ -0,0 +1,17 @@
+<!--
+ @Name
+ @Description
+ @author PerkinsZhu
+ @date 2023/6/28 16:17
+-->
+<script setup lang="ts">
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped lang="scss">
+
+</style>
