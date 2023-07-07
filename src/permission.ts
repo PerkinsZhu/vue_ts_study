d00cae01bf6b3ff89a8745ea2a1633fbb299733a
PerkinsZhu <perkinszhu@sina.cn>
2023-07-07 14:09:18 +0800

    实现登录状态拦截，强制跳转login页面

--- src/permission.ts
+++ src/permission.ts
@@ -9,10 +9,30 @@ import router from "@/router";
 import nprogress from "nprogress";
 import "nprogress/nprogress.css";
 
+import pinia from "@/store";
+import useUserStore from "@/store/modules/user.ts";
+
+const userStore = useUserStore(pinia);
+
 router.beforeEach((to, from, next) => {
   console.log("beforeEach", to, from);
+
+  console.log(userStore.token);
+  if (userStore.token) {
+    if (to.path === "/login") {
+      next({ path: "/home" });
+    } else {
+      next();
+    }
+  } else {
+    if (to.path === "/login") {
+      next();
+    } else {
+      next({ path: "/login" });
+    }
+
+  }
   nprogress.start();
-  next();
 });
 
 router.afterEach((to, from) => {
