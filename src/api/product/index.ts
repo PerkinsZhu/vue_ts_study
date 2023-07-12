94898fffb95a9c248c9e295b1cd1d107d43356e9
PerkinsZhu <perkinszhu@sina.cn>
2023-07-12 10:28:50 +0800

    品牌支持新增和编辑

--- src/api/product/index.ts
+++ src/api/product/index.ts
@@ -9,7 +9,17 @@ import request from "@/utils/request.ts";
 
 enum API {
   TRADEMARK_LIST = "/admin/product/baseTrademark",
+  TRADEMARK_SAVE = "/admin/product/baseTrademark/save",
+  TRADEMARK_UPDATE = "/admin/product/baseTrademark/update",
 }
 
 export const trademarkList = (page: number, limit: number) =>
   request.get<any, any>(API.TRADEMARK_LIST + "/" + page + "/" + limit);
+
+export const trademarkSaveOrUpdate = (data: any) => {
+  if (data.id) {
+    return request.put<any, any>(API.TRADEMARK_UPDATE, data);
+  } else {
+    return request.post<any, any>(API.TRADEMARK_SAVE, data);
+  }
+};
--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -6,9 +6,9 @@
 -->
 <script setup lang="ts">
 import { Delete, Edit } from "@element-plus/icons-vue";
-import { trademarkList } from "@/api/product/index.ts";
-import { ElNotification } from "element-plus";
-import { ref } from "vue";
+import { trademarkList, trademarkSaveOrUpdate } from "@/api/product/index.ts";
+import { ElMessage, ElNotification, UploadProps } from "element-plus";
+import { ref, computed } from "vue";
 
 const tableData = ref([]);
 
@@ -42,10 +42,79 @@ function limitChange(current: number) {
   limit.value = current;
   loadList();
 }
+
+const form = ref({
+  id: null,
+  tmName: "",
+  logoUrl: ""
+});
+
+let imageUrl = computed(() => {
+  return form.value.logoUrl;
+});
+
+let dialogFormVisible = ref(false);
+
+function addTrademark() {
+  console.log(form.value);
+  dialogFormVisible.value = true;
+}
+
+function edit(row) {
+  dialogFormVisible.value = true;
+  form.value = {
+    id: row.id,
+    tmName: row.tmName,
+    logoUrl: row.logoUrl
+  };
+}
+
+const beforeAvatarUpload: UploadProps["beforeUpload"] = (rawFile) => {
+  console.log(rawFile.type);
+  if (rawFile.type !== "image/jpeg" && rawFile.type !== "image/png") {
+    ElMessage.error("图片只能是 JPG 格式!");
+    return false;
+  } else if (rawFile.size / 1024 / 1024 > 2) {
+    ElMessage.error("图片大小不能超过 2MB!");
+    return false;
+  }
+  return true;
+};
+
+const handleAvatarSuccess: UploadProps["onSuccess"] = (response, uploadFile) => {
+  form.value.logoUrl = response.data;
+};
+
+function close() {
+  dialogFormVisible.value = false;
+}
+
+function submit() {
+  trademarkSaveOrUpdate(form.value).then((res) => {
+    if (res.code === 200) {
+      ElNotification({
+        type: "success",
+        message: res.message
+      });
+      dialogFormVisible.value = false;
+      form.value = {
+        id: null,
+        tmName: "",
+        logoUrl: ""
+      };
+      loadList();
+    } else {
+      ElNotification({
+        type: "error",
+        message: res.message
+      });
+    }
+  });
+}
 </script>
 
 <template>
-  <el-button type="success">添加品牌</el-button>
+  <el-button type="success" @click="addTrademark">添加品牌</el-button>
   <el-table :data="tableData" border style="width: 100%">
     <el-table-column type="index" label="序号" width="100px" />
     <el-table-column prop="tmName" label="品牌名称" />
@@ -59,10 +128,10 @@ function limitChange(current: number) {
       </template>
     </el-table-column>
     <el-table-column prop="address" label="品牌操作">
-      <el-row>
-        <el-button type="primary" :icon="Edit" circle />
+      <template #="{ row, $index }">
+        <el-button type="primary" :icon="Edit" circle @click="edit(row)" />
         <el-button type="danger" :icon="Delete" circle />
-      </el-row>
+      </template>
     </el-table-column>
   </el-table>
   <el-pagination
@@ -78,8 +147,68 @@ function limitChange(current: number) {
     prev-text="上一页"
 
   />
+
+  <el-dialog v-model="dialogFormVisible" title="新增品牌">
+    <el-form :model="form">
+      <el-form-item label="品牌名称" label-width="80px">
+        <el-input v-model="form.tmName" autocomplete="off" style="width: 80%" />
+      </el-form-item>
+      <el-form-item label="品牌LOGO" label-width="80px">
+        <el-upload
+          class="avatar-uploader"
+          action="/api/admin/product/fileUpload"
+          :show-file-list="false"
+          :before-upload="beforeAvatarUpload"
+          :on-success="handleAvatarSuccess"
+        >
+          <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+          <el-icon v-else class="avatar-uploader-icon">
+            <Plus />
+          </el-icon>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="close">关闭</el-button>
+        <el-button type="primary" @click="submit">保存</el-button>
+      </span>
+    </template>
+  </el-dialog>
+
 </template>
 
 <style scoped lang="scss">
 
+</style>
+
+<style scoped>
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
 </style>
